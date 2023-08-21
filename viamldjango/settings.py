a0b31edb2b269de54a165816ec1bc6963d38753f
mdkashifsidd2020 <142329771+mdkashifsidd2020@users.noreply.github.com>
2023-08-21 14:31:55 +0530

    Update settings.py

--- viamldjango/settings.py
+++ viamldjango/settings.py
@@ -131,6 +131,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
