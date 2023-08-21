e38ba023fa68537d7ba19e2433aaaa5f0ebf4783
mdkashifsidd2020 <142329771+mdkashifsidd2020@users.noreply.github.com>
2023-08-21 14:28:27 +0530

    Update urls.py

--- viamldjango/urls.py
+++ viamldjango/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 # from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 # from django.conf import settings
 
 urlpatterns = [
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
